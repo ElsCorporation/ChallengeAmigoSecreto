09cc2d9ef01ab9fe63baaca37366d365774567f1
Edinho Lima dos Santos <edinho.lima.santos@gmail.com>
2025-01-21 20:12:29 -0300

    Primeiros Passos da funcao adicionarAmigo.

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ let controladorNomes = 0 ;
 function adicionarAmigo() {
     console.log("Entrei no controladorNomes");
     let textoValido = document.getElementById("amigo").value;
-    console.log(`textoValido: ${textoValido}`);
-        if (textoValido === null){
-            //console.log(textoValido.value);
+
+        if (textoValido == "") {
+
             alert("Por favor, digite um nome!");
 
         } else {
+
                 console.log("O texto é válido");
+
                 while(controladorNomes >= 0 ){
+                    
                     console.log("Entrei no controladorNomes");
-                amigoSorteado[controladorNomes]=textoValido;
-                controladorNomes++;
-                console.log(`Nomes Armazenados: ${amigoSorteado}`);
-                break;
+                    amigoSorteado[controladorNomes]=textoValido;
+                    controladorNomes++;
+                    console.log(`Nomes Armazenados: ${amigoSorteado} `);
+                    break;
                 
                 }
             
