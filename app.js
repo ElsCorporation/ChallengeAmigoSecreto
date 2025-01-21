4ae6882b9b37351613ecd55ce94164ff60d3d5e4
Edinho Lima dos Santos <edinho.lima.santos@gmail.com>
2025-01-21 19:19:28 -0300

    Implementacao Inicial da funcao adicionarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 let amigoSorteado = [];
+let controladorNomes = 1 ;
+
+function adicionarAmigo() {
+    console.log("Entrei no controladorNomes");
+    
+        if (document.HTMLInputElement == null){
+            console.log(document.HTMLInputElement);
+            alert("Por favor, digite um nome!");
+
+        } else {
+                amigoSorteado[controladorNomes]=document.querySelector("text").value;
+                controladorNomes++;
+                console.log(`Nomes Armazenados: ${amigoSorteado}`);
+        }
+        
+    
+}
